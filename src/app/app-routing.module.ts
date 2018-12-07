f05a082e1e1cdf8649408a8f684e1ff72dc03b20
Jia Xiang Cheng <jiaxiangcheng7@gmail.com>
2018-12-07 19:04:44 +0100

    add google login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from '../app/components/index/index.component';
 import { NewestComponent } from '../app/components/newest/newest.component';
 import { AskComponent } from './components/ask/ask.component';
+import { SigninComponent } from './components/signin/signin.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/index', pathMatch: 'full' },
     { path: 'index', component: IndexComponent },
     { path: 'newest', component: NewestComponent },
-    { path: 'ask', component: AskComponent}
+    { path: 'ask', component: AskComponent},
+    { path: 'login', component: SigninComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { SigninComponent } from './components/signin/signin.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './components/index/index.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { NewestComponent } from './components/newest/newest.component';
 import { AskComponent } from './components/ask/ask.component';
+import {
+    SocialLoginModule,
+    AuthServiceConfig,
+    GoogleLoginProvider,
+    FacebookLoginProvider
+} from 'angular-6-social-login';
+
+// Configs
+export function getAuthServiceConfigs() {
+    const config = new AuthServiceConfig([
+        {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('652642074981-u3scjh400dkp6116b6pj4fpgth17eod5.apps.googleusercontent.com')
+        }
+    ]);
+    return config;
+}
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    IndexComponent,
-    TopBarComponent,
-    NewestComponent,
-    AskComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        IndexComponent,
+        TopBarComponent,
+        NewestComponent,
+        AskComponent,
+        SigninComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        SocialLoginModule
+    ],
+    providers: [
+        {
+            provide: AuthServiceConfig,
+            useFactory: getAuthServiceConfigs
+        }
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
